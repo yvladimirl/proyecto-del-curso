1bdce61ae43f9ddc51c933ad0dff43bdd903a04b
YVladimirLlanosA <yvladimir.llanosa@gmail.com>
2023-06-20 18:08:28 -0400

    Realizacion de la tarea de EDteam, CSS e incremento de valores de la peticion GET

--- src/App1.jsx
+++ src/App1.jsx
@@ -3,32 +3,40 @@ import axios from "axios"
 import "./App.css"
 
 function App1() {
-  const API_URL = import.meta.env.VITE_API_URL
+    const API_URL = import.meta.env.VITE_API_URL
 
-  const [criptos,setCriptos] = useState()
+    const [criptos, setCriptos] = useState()
 
-  useEffect(() => {
-    axios.get(`${API_URL}assets`)
-    .then((data) => {
-          setCriptos(data.data.data)
-    })
-    .catch(() => {
-      console.error("La petición falló.")
-    })
-  }, [])
+    useEffect(() => {
+        axios.get(`${API_URL}assets`)
+            .then((data) => {
+                setCriptos(data.data.data)
+            })
+            .catch(() => {
+                console.error("La petición falló.")
+            })
+    }, [])
 
-  if(!criptos) return <span>Cargando ...</span>
+    if (!criptos) return <span>Cargando ...</span>
 
- return (
-    <>
-     <h1>Lista de criptomonedas</h1>
-     <ol>
-        { criptos.map(({id, name, priceUsd}) => (
-          <li key={id}>Nombre: {name} Precio: {priceUsd}</li>
-        ))}
-     </ol>
-    </>
-  )
+    return (
+        <>
+            <h1>Lista de criptomonedas</h1>
+            <ol>
+                {criptos.map(({ id, rank, symbol, name, supply, maxsupply, priceUsd }) => (
+                    <li key={id}>
+                        <div>Id: {id}</div>
+                        <div>Ranking: {rank}</div>
+                        <div>Simbolo: {symbol}</div>
+                        <div>Nombre: {name}</div>
+                        <div>Suministro: {supply}</div>
+                        <div>Max. Suministro: {maxsupply}</div>
+                        <div>Precio: {priceUsd}</div>
+                    </li>
+                ))}
+            </ol>
+        </>
+    )
 }
 
 export default App1
